34c2e5f4d821f0b79e212d0bf17459b22117d7a5
Shivang Shukla <shuklashivang036@gmail.com>
2021-07-08 15:26:52 +0530

    Update caesar_cipher.rb

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -2,14 +2,17 @@ def caesar_cipher(string,number)
   letter = string.split("")
   letter.map! do |character|
     if ((character.ord >= 'a'.ord && character.ord <= 'z'.ord) || (character.ord >= 'A'.ord && character.ord <= 'Z'.ord) )
+      #Check whether the character is a letter or symbol
       character = character.ord
       character = character + number
       if (character > 'z'.ord)
+        #wrapping z to a
         character = character - 'z'.ord
         character = ('a'.ord - 1) + character
         character = character.chr
         elsif(character > 'Z'.ord && character < 'a'.ord)
-      character = character - 'Z'.ord
+        #wrapping Z to A
+        character = character - 'Z'.ord
         character = ('A'.ord - 1) + character
         character = character.chr
       else
