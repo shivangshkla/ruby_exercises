eb52212093108e870c0038e8410711cffbf07e6c
Shivang Shukla <shuklashivang036@gmail.com>
2021-07-10 15:09:32 +0530

    Update caesar_cipher.rb

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,27 +1,22 @@
 def caesar_cipher(string,number)
-  letter = string.split("")
-  letter.map! do |character|
-    if ((character.ord >= 'a'.ord && character.ord <= 'z'.ord) || (character.ord >= 'A'.ord && character.ord <= 'Z'.ord) )
-      #Check whether the character is a letter or symbol
-      character = character.ord
+  letter = string.split('')
+  letter.map! do |character| 
+    if(character >= 'a' && character <= 'z')
+      character = character.ord - 'a'.ord 
       character = character + number
-      if (character > 'z'.ord)
-        #wrapping z to a
-        character = character - 'z'.ord
-        character = ('a'.ord - 1) + character
-        character = character.chr
-        elsif(character > 'Z'.ord && character < 'a'.ord)
-        #wrapping Z to A
-        character = character - 'Z'.ord
-        character = ('A'.ord - 1) + character
-        character = character.chr
-      else
+      character =  character % 26
+      character =  'a'.ord  + character
       character = character.chr
-      end
+      elsif(character >= 'A' && character <= 'Z')
+      character = character.ord - 'A'.ord 
+      character = character + number
+      character =  character % 26
+      character =  'A'.ord  + character
+      character = character.chr
     else
     character = character
     end
   end
-puts string = letter.join("")
-  end
+ puts string = letter.join('')
+end
 caesar_cipher("What a string!",5)
